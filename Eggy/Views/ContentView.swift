236d84738c35610d0dadec0826ad125bbc948251
ericlewis <ericlewis777@gmail.com>
2019-07-08 11:30:13 -0400

    wobble

--- Eggy/Views/ContentView.swift
+++ Eggy/Views/ContentView.swift
@@ -15,63 +15,59 @@ struct ContentView : View {
     @EnvironmentObject private var store: EggManager
     
     // MARK: Render
-  
-  
-  enum DragState {
     
-    case inactive
-    case dragging(translation: CGSize)
     
-    var translation: CGSize {
-      switch self {
-      case .inactive:
-        return .zero
-      case .dragging(let translation):
-        return translation
-      }
+    enum DragState {
+        
+        case inactive
+        case dragging(translation: CGSize)
+        
+        var translation: CGSize {
+            switch self {
+            case .inactive:
+                return .zero
+            case .dragging(let translation):
+                return translation
+            }
+        }
+        
+        var isActive: Bool {
+            switch self {
+            case .inactive:
+                return false
+            case .dragging:
+                return true
+            }
+        }
     }
     
-    var isActive: Bool {
-      switch self {
-      case .inactive:
-        return false
-      case .dragging:
-        return true
-      }
+    var dragOpacity: Double {
+        dragState.isActive ? 0.0 : 1.0
     }
-  }
-  
-  var dragOpacity: Double {
-     dragState.isActive ? 0.0 : 1.0
-  }
-  
-  
-  @GestureState var dragState = DragState.inactive
+    
+    
+    @GestureState var dragState = DragState.inactive
     
     var body: some View {
-      let dragGester = DragGesture()
-        .updating($dragState) { (value, state, transaction) in
-          state = .dragging(translation: value.translation)
-      }
-       return VStack {
-            EggStack()
-              .tapAction(store.toggleRunning)
-              .offset(
-                x: dragState.translation.width,
-                y: dragState.translation.height
-              )
-              .gesture(dragGester)
-
+        let dragGesture = DragGesture()
+            .updating($dragState) { (value, state, transaction) in
+                state = .dragging(translation: value.translation)
+        }
+        return VStack {
+            EggStack(x: dragState.translation.width, y: dragState.translation.height, isDragging: dragState.isActive)
+                .tapAction(store.toggleRunning)
+                .gesture(dragGesture)
+            
             if !store.isRunning {
                 OptionSliders()
-                  .opacity(dragOpacity)
-                  .transition(.opacity)
-                  .onReceive(store.ticker, perform: {
-                    if self.store.isRunning {
-                      self.store.changed()
-                    }
-                  })
-              
+                    .opacity(dragOpacity)
+                    .transition(.opacity)
+                    .onReceive(store.ticker, perform: {
+                        if self.store.isRunning {
+                            self.store.changed()
+                        }
+                    })
+                
             } else {
                 ProjectedEndLabel()
                     .transition(.moveAndFade)
